da1a594cc4d838d89dd689799b39847dacfe2dad
alphajuliet <andrew@alphajuliet.com>
2020-06-07 16:25:22 +1000

    Add flow sketch. First pass on implementing claim-route-options.

--- src/ttr/game.clj
+++ src/ttr/game.clj
@@ -19,6 +19,7 @@
   (->> (st/empty-state nplayers)
        (act/deal-table)))
 
+;;-------------------------------
 ;; take-card-options :: Player -> State -> [Action]
 (defn take-card-options
   "Enumerate all the actions for a player to take cards."
@@ -28,9 +29,55 @@
    (for [c (num/map-enumerate (get state :cards))]
      `(act/take-card ~player ~c))))
 
-;; claim-route-options :: Player -> State -> [Action]
-;; available-actions :: Player -> State -> [Action]
+;;-------------------------------
+#_(defn- route->map
+    "Turn a route into a proper map."
+    [r]
+    (into (last r) {:src (first r), :dest (second r)}))
 
+(defn- get-eligible-routes
+  "Get all the eligible routes based on the number of each colour card in the hand."
+  [player state]
+  (for [[colour n] (get-in state [:player player :cards])
+        r (range 1 (inc n))
+        :when (not (= colour :loco))]
+    (let [r1 (gr/get-routes (:map state) {:colour colour
+                                          :length r
+                                          :claimed-by nil})
+          ; And get all the uncoloured routes too
+          r2 (gr/get-routes (:map state) {:colour :none
+                                          :length r
+                                          :claimed-by nil})]
+      (concat r1 r2))))
+
+(defn- routes->actions
+  "Turn the routes into claim-route actions. 
+   Note that the chosen-colour for all routes is :none. A policy needs to be invoked by the consumer on the resulting actions to override this for uncoloured routes."
+  [player routes]
+  (for [r routes]
+    `(act/claim-route  ~player :none ~r)))
+
+;; claim-route-options :: Player -> State -> [Action]
+(defn claim-route-options
+  "Enumerate all the claimable routes based on the hand cards and what's not yet claimed."
+  [player state]
+  (->> state
+   (get-eligible-routes player)
+   (apply concat) ;flatten one level
+   (dedupe)
+   (routes->actions player)))
+
+;;-------------------------------
+;; available-actions :: Player -> State -> [Action]
+(defn available-actions
+  "Return all the available actions, given a player and a current state."
+  [player state]
+  (reduce concat
+          '()
+          ((juxt take-card-options claim-route-options)
+           player state)))
+
+;;-------------------------------
 ;; apply-action :: Action -> State -> State
 (defn apply-action
   "Apply an action to a state"
--- test/ttr/game-test.clj
+++ test/ttr/game-test.clj
@@ -1,14 +1,21 @@
 (ns ttr.game-test
   (:require [clojure.test :refer [deftest is testing]]
-            [ttr.graph :as gr]
+            #_[ttr.graph :as gr]
             [ttr.num-map :as num]
-            [ttr.state :as st]
-            [ttr.action :as act]
+            #_[ttr.state :as st]
+            #_[ttr.action :as act]
             [ttr.game :as game]))
 
 (deftest test-take-card-options
-  (let [s0 (game/init-game 2)]
-    (is (= 6 (count (game/take-card-options 0 s0))))))
+  (testing "All actions for taking cards."
+    (let [s0 (game/init-game 2)]
+     (is (= 6 (count (game/take-card-options 0 s0)))))))
 
+(deftest test-claim-route-options
+  (testing "All claimable routes."
+    (let [s0 (game/init-game 2)
+          s1 (update-in s0 [:player 0 :cards]
+                        (partial num/map-add {:red 1 :blue 3}))]
+      (is (= 46 (count (game/claim-route-options 0 s1)))))))
 
 ;; The End
